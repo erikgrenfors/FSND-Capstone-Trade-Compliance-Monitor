30bee11e8ef4687cb9bcf4c809ee68a8bab97cc3
Erik Grenfors <>
2020-07-21 21:08:15 +0200

    Add tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,4 +1,5 @@
 import os
+import time
 import unittest
 
 from tcm_app import create_app
@@ -41,9 +42,10 @@ class TradeComplianceMonitor(unittest.TestCase):
         self.app_context.push()
         db.drop_all()
         db.create_all()
+        time.sleep(5)  # To stay clear of 429 response when requesting userinfo
 
     def test_post_trades(self):
-        body = self.trade_json
+        body = self.trade_json.copy()
         res = self.client.post('/api/trades', headers=self.headers, json=body)
         self.assertEqual(res.status_code, 200)
 
@@ -78,23 +80,126 @@ class TradeComplianceMonitor(unittest.TestCase):
     def test_get_trades(self):
         res = self.client.get('/api/trades', headers=self.headers)
         self.assertEqual(res.status_code, 204)
-        body = self.trade_json
+        # Use Employee reporting two trades
+        body = self.trade_json.copy()
         res = self.client.post('/api/trades', headers=self.headers, json=body)
         res = self.client.get('/api/trades', headers=self.headers)
         self.assertEqual(res.status_code, 200)
 
+    def test_get_trade(self):
+        res = self.client.get('/api/trades/1', headers=self.co_headers)
+        self.assertEqual(res.status_code, 404)
+
+        # Use CO as Employee reporting a violating trade (buy and sell)
+        body = self.trade_json.copy()
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        res = self.client.get('/api/trades/1', headers=self.co_headers)
+        self.assertEqual(res.status_code, 200)
+        # ... which other Employees cant see.
+        self.client.cookie_jar.clear()
+        res = self.client.get('/api/trades/1', headers=self.headers)
+        self.assertEqual(res.status_code, 404)
+
+    def test_patch_trade(self):
+        body = self.trade_json.copy()
+        res = self.client.patch('/api/trades/1', headers=self.co_headers, json=body)
+        self.assertEqual(res.status_code, 404)
+
+        # Use CO as Employee reporting a trade to patch
+        body = self.trade_json.copy()
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        res = self.client.patch('/api/trades/1', headers=self.co_headers, json=body)
+        self.assertEqual(res.status_code, 204)
+
+        body['quantity'] = 50
+        res = self.client.patch('/api/trades/1', headers=self.co_headers, json=body)
+        self.assertEqual(res.status_code, 200)
+
+        # ... which other Employees cant change.
+        self.client.cookie_jar.clear()
+        res = self.client.patch('/api/trades/1', headers=self.headers, json=body)
+        self.assertEqual(res.status_code, 404)
+
+    def test_delete_trade(self):
+        res = self.client.delete('/api/trades/1', headers=self.co_headers)
+        self.assertEqual(res.status_code, 404)
+
+        # Use CO as Employee reporting two trades
+        body = self.trade_json.copy()
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        # ... which he/she can delete ...
+        res = self.client.delete('/api/trades/1', headers=self.co_headers)
+        self.assertEqual(res.status_code, 204)
+        # ... but other Employees cant delete.
+        self.client.cookie_jar.clear()
+        res = self.client.delete('/api/trades/2', headers=self.headers)
+        self.assertEqual(res.status_code, 404)
+
+        # Use Employee reporting two trades
+        res = self.client.post('/api/trades', headers=self.headers, json=body)
+        res = self.client.post('/api/trades', headers=self.headers, json=body)
+        # ... which he/she can delete ...
+        res = self.client.delete('/api/trades/3', headers=self.headers)
+        self.assertEqual(res.status_code, 204)
+        # ... but which not even a CO can delete.
+        self.client.cookie_jar.clear()
+        res = self.client.delete('/api/trades/4', headers=self.co_headers)
+        self.assertEqual(res.status_code, 404)
+
     def test_get_violations(self):
         res = self.client.get('/api/violations', headers=self.headers)
         self.assertEqual(res.status_code, 204)
-        # Create violating closed trade
-        body = self.trade_json
+
+        # Use Employee reporting a violating trade (buy and sell)
+        body = self.trade_json.copy()
         res = self.client.post('/api/trades', headers=self.headers, json=body)
         body['date'] = '2020-01-15'
         body['price'] = 375
         body['direction'] = 'Sell'
         res = self.client.post('/api/trades', headers=self.headers, json=body)
-        # Now there are violations
+        # Now there are a violation ...
         res = self.client.get('/api/violations', headers=self.headers)
+        self.assertEqual(res.status_code, 200)
+        # ... which other Employees cant see.
+
+        self.client.cookie_jar.clear()
+        res = self.client.get('/api/violations', headers=self.co_headers)
+        self.assertEqual(res.status_code, 204)
+
+
+    def test_get_all_violations(self):
+        res = self.client.get('/api/all-violations', headers=self.co_headers)
+        self.assertEqual(res.status_code, 204)
+
+        # Use CO as Employee reporting a violating trade (buy and sell)
+        body = self.trade_json.copy()
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        body['date'] = '2020-01-15'
+        body['price'] = 375
+        body['direction'] = 'Sell'
+        res = self.client.post('/api/trades', headers=self.co_headers, json=body)
+        # Now there are a violation ...
+        res = self.client.get('/api/all-violations', headers=self.co_headers)
+        self.assertEqual(res.status_code, 200)
+        # ... which other Employees cant see.
+        self.client.cookie_jar.clear()
+        res = self.client.get('/api/all-violations', headers=self.headers)
+        self.assertEqual(res.status_code, 403)
+
+        # Use Employee reporting a violating trade (buy and sell)
+        body = self.trade_json.copy()
+        res = self.client.post('/api/trades', headers=self.headers, json=body)
+        body['date'] = '2020-01-15'
+        body['price'] = 375
+        body['direction'] = 'Sell'
+        res = self.client.post('/api/trades', headers=self.headers, json=body)
+        # Now there are violations for which a CO can see
+        # (delete his/her own first)
+        self.client.cookie_jar.clear()
+        res = self.client.delete('/api/trades/1', headers=self.co_headers)
+        res = self.client.delete('/api/trades/2', headers=self.co_headers)
+        res = self.client.get('/api/all-violations', headers=self.co_headers)
         self.assertEqual(res.status_code, 200)
 
 
